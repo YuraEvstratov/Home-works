6548264b7736e03dfa4a0f033b3a0c3f6c4907d5
YuraEvstratov <yueaevstratov@mail.ru>
2026-05-11 15:05:43 +0700

    class List

---
+++ Tasks/class LIst.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data: int):
+        self.data = data
+        self.next = None
+        self.prev = None
+class List:       
+    def __init__(self):
+        self.head = None
+        self.size = 0
+        
+    def append(self, data: int):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current = self.head
+        while current.next:
+            current.prev = current
+            current = current.next
+        current.next = new_node
+
+        self.size += 1
+
+    def appstart(self, data: int):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        current = new_node
+        current.next = self.head
+        self.head = current
+        self.size += 1
+    
+    def print(self):
+        current = self.head
+        while current.next:
+            print(current.data)
+            current = current.next
+        
+l = List()
+for i in range(5):
+    l.append(i)
+l.print()
+    
+
+        
